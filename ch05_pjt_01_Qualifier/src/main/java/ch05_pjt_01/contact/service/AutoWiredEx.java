cec6cb9b092382b11b3ad07b5e288692bf617883
kimkw1110 <kyungwan3014@naver.com>
2024-05-31 00:40:19 +0900

    ch05_pjt_01_Qualifier

---
+++ ch05_pjt_01_Qualifier/src/main/java/ch05_pjt_01/contact/service/AutoWiredEx.java
@@ -0,0 +1,19 @@
+package ch05_pjt_01.contact.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class AutoWiredEx {
+	
+	@Autowired
+	public AutoWiredEx(@Qualifier("qualifierBean") FirstBean fBean, SecondBean sBean) {
+		System.out.println("firstBean: " + fBean);
+		System.out.println("secondBean: " + sBean);
+	}    
+	
+	@Autowired
+	public void autowiredMethod(ThirdBean tBean, @Qualifier("qualifierBean") FourthBean fBean) {
+		System.out.println("thirdBean: " + tBean);
+		System.out.println("fourthBean: " + fBean);
+	}
+}
